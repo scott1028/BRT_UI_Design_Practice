53999a72bba759a54f9e5d2c8ccbafc93d31ec58
scott1028 <mic1028002@gmail.com>
2013-09-27 12:54:33 +0800

    modified:   modules/bt10.js 	modified:   modules/init.css 	deleted:    "\350\252\252\346\230\216\346\226\207\344\273\266/~$\351\253\224\346\270\254\350\251\246\350\246\217\345\212\203\346\233\270.docx"

--- modules/bt10.js
+++ modules/bt10.js
@@ -4,5 +4,51 @@ var bt10=function(){
 	.append('<li class="active"><a href="#">系統帳號管理</a></li>');
 
 	$('table td.bottomInfo').hide();//.css('height','200px');
-	qmap=$('#centerview').empty();
+	qmap=$('#centerview').empty().css('padding','10px');
+
+	var topbutton=$('\
+		<div style="text-align:right;margin-bottom:10px;height:30px;">\
+			<span class="btn btn-inverse">新增使用者</span>\
+		</div>\
+	').appendTo(qmap);
+
+	var datagrid=$('\
+		<table class="table table-bordered table-hover table-usergrid">\
+			<thead>\
+				<tr>\
+					<th style="width:40px;text-align:center;">編號</th>\
+					<th style="text-align:center;">帳號</th>\
+					<th style="width:120px;text-align:center;">管理</th>\
+				</tr>\
+			</thead>\
+			<tbody>\
+				<tr>\
+					<th style="text-align:right;">1</th>\
+					<td>user1@brt.com</td>\
+					<td style="text-align:center;">\
+						<span class="btn" style="width:30px;">修改</span>\
+						<span class="btn btn-danger" style="width:30px;">刪除</span>\
+					</td>\
+				</tr>\
+				<tr>\
+					<th style="text-align:right;">2</th>\
+					<td>user2@brt.com</td>\
+					<td style="text-align:center;">\
+						<span class="btn" style="width:30px;">修改</span>\
+						<span class="btn btn-danger" style="width:30px;">刪除</span>\
+					</td>\
+				</tr>\
+				<tr>\
+					<th style="text-align:right;">3</th>\
+					<td>user3@brt.com</td>\
+					<td style="text-align:center;">\
+						<span class="btn" style="width:30px;">修改</span>\
+						<span class="btn btn-danger" style="width:30px;">刪除</span>\
+					</td>\
+				</tr>\
+			</tbody>\
+		</table>\
+	').appendTo(qmap);
+
+	$('#centerview').hide().fadeIn(300);
 }
