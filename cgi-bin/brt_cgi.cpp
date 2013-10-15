760b3c3043efcfa3422e7ede8782486689f13c54
scott1028 <mic1028002@gmail.com>
2013-10-15 15:26:02 +0800

    modified:   cgi-bin/brt_cgi.cpp

--- cgi-bin/brt_cgi.cpp
+++ cgi-bin/brt_cgi.cpp
@@ -22,15 +22,14 @@ static int callback_json(void *NotUsed, int argc, char **argv, char **azColName)
 void do_sql_json(string sql, int rc, char *zErrMsg, sqlite3 *db){
 	rc = sqlite3_exec(db, sql.c_str(), callback_json, 0, &zErrMsg);
 	if( rc != SQLITE_OK ){
-			//fprintf(stderr, "SQL error: %s\n", zErrMsg);
-			sqlite3_free(zErrMsg);
+		//fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
 	}else{
-			//fprintf(stdout, "SQL Command successfully\n");
+		//fprintf(stdout, "SQL Command successfully\n");
 	}
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
 	sqlite3 *db;
 	char *zErrMsg = 0;
 	int rc;
@@ -47,39 +46,39 @@ int main(int argc, char* argv[])
 		//fprintf(stderr, "Opened database successfully\n");
 	}
 	
-	sql = "CREATE TABLE COMPANY("\
-		"ID				INTEGER	NOT NULL,"\
-		"NAME			TEXT    NOT NULL,"\
-		"AGE            INT     NOT NULL,"\
-		"ADDRESS        CHAR(50),"\
-		"SALARY         REAL,"\
-		"PRIMARY KEY (\"ID\")"\
-	");";
-	do_sql_json(sql, rc, zErrMsg, db);	// Create
+	// sql = "CREATE TABLE COMPANY("\
+	// 	"ID				INTEGER	NOT NULL,"\
+	// 	"NAME			TEXT    NOT NULL,"\
+	// 	"AGE            INT     NOT NULL,"\
+	// 	"ADDRESS        CHAR(50),"\
+	// 	"SALARY         REAL,"\
+	// 	"PRIMARY KEY (\"ID\")"\
+	// ");";
+	// do_sql_json(sql, rc, zErrMsg, db);	// Create
 	
-	for(int i=0;i<10;i++){
-		sprintf(i2s, "INSERT INTO COMPANY (NAME,AGE,ADDRESS,SALARY) VALUES ('Paul', %d, 'California', 20000.00 );" ,i);
-		sql=string(i2s);
-		do_sql_json(sql, rc, zErrMsg, db);	// Create
-	}
+	// for(int i=0;i<10;i++){
+	// 	sprintf(i2s, "INSERT INTO COMPANY (NAME,AGE,ADDRESS,SALARY) VALUES ('Paul', %d, 'California', 20000.00 );" ,i);
+	// 	sql=string(i2s);
+	// 	do_sql_json(sql, rc, zErrMsg, db);	// Create
+	// }
 	
-	sql = "SELECT * from COMPANY";
-	cout << "Content-Type: text/json" << endl << endl;
-	do_sql_json(sql, rc, zErrMsg, db);	// Read
+	// sql = "SELECT * from COMPANY";
+	// cout << "Content-Type: text/json" << endl << endl;
+	// do_sql_json(sql, rc, zErrMsg, db);	// Read
 
-	// Post Data Handle (與 Get Handle 不能共存)
-	int contentLength = atoi( getenv( "CONTENT_LENGTH" ) );
-	char *postString=new char[contentLength+1];
-	cin.read( postString, contentLength );
-	postString[contentLength] = '\0';		// set EOF point by yourself...
-	cout << "[";
-	cout << "{ \"post\" : \"" << postString << "\" }";
-	cout << ",";
-	cout << "{ \"len\" : \"" << contentLength << "\" }";
-	cout << "]" << endl;
+	// // Post Data Handle (與 Get Handle 不能共存)
+	// int contentLength = atoi( getenv( "CONTENT_LENGTH" ) );
+	// char *postString=new char[contentLength+1];
+	// cin.read( postString, contentLength );
+	// postString[contentLength] = '\0';		// set EOF point by yourself...
+	// cout << "[";
+	// cout << "{ \"post\" : \"" << postString << "\" }";
+	// cout << ",";
+	// cout << "{ \"len\" : \"" << contentLength << "\" }";
+	// cout << "]" << endl;
 	
-	// Get Data Handle (與 Post Handle 不能共存)
-	//cout << "[{ \"get\" : \"" << getenv("QUERY_STRING") << "\" }]";
+	// // Get Data Handle (與 Post Handle 不能共存)
+	// //cout << "[{ \"get\" : \"" << getenv("QUERY_STRING") << "\" }]";
 	
 	sqlite3_close(db);
 	return 0;
